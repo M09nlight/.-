10df126ccb80314bcf8640b2f3422a49def98146
M09nlight <ivan.portyanko@yandex.ru>
2024-01-19 21:41:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import mp3 from './love.mp3';
+
+function App() {
+  const music = useRef();
+  const [play, setPlay] = useState(false);
+
+  const changeMusicStatus = () => {
+    if (!play) {
+      music?.current?.play();
+      setPlay(true);
+      music.current.volume = 0.02;
+    } else {
+      music?.current?.pause();
+      setPlay(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="title" onClick={changeMusicStatus}>
+        Eilmoon
+      </div>
+      <div className="button">
+        <a href="https://instagram.com/eilmoon" target="blank" className="link">
+          inst
+        </a>
+      </div>
+      <div className="button">
+        <a
+          href="https://t.me/+a3EyxtojIfEyMzli"
+          target="blank"
+          className="link"
+        >
+          tg
+        </a>
+      </div>
+      <div className="button">
+        <a
+          href="https://www.donationalerts.com/r/eilmoon"
+          target="blank"
+          className="link"
+        >
+          donationalerts
+        </a>
+      </div>
+
+      <audio controls src={mp3} ref={music} style={{ display: 'none' }}></audio>
+    </div>
+  );
+}
+
+export default App;
