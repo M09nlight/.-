e5f6f555593e7dcad28f0a2ec69e59100ac90272
m09nlight <ivan.portyanko1609@gmail.com>
2024-03-21 16:44:01 +0300

    feat: added birthday component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useRef, useState } from "react";
 import "./App.css";
 import LinksInfo from "./components/LinksInfo/LinksInfo";
 import { Flowers } from "./components/Flowers/Flowers";
+import BirthDay from "./components/BirthDay/BirthDay";
 
 function App() {
   const date = new Date();
   const isWomanDay = date.getMonth() === 2 && date.getDate() === 8;
-  return <>{!isWomanDay ? <LinksInfo /> : <Flowers />}</>;
+  const isBirthday = date.getMonth() === 2 && date.getDate() === 24;
+  if (isWomanDay) {
+    return <Flowers />;
+  }
+  if (isBirthday) {
+    return <BirthDay />;
+  }
+  return <LinksInfo />;
 }
 
 export default App;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,11 +1,17 @@
 import React, { useRef, useState } from "react";
 
-export default function Title({ isAbsolute, mp3 }) {
+export default function Title({
+  isAbsolute,
+  isBirthday,
+  mp3,
+  text = "Eilmoon",
+}) {
   const music = useRef();
   const [play, setPlay] = useState(false);
 
   const changeMusicStatus = () => {
     if (!play) {
+      console.log("change");
       music?.current?.play();
       setPlay(true);
       music.current.volume = 0.02;
@@ -14,15 +20,27 @@ export default function Title({ isAbsolute, mp3 }) {
       setPlay(false);
     }
   };
+  const handleMusicEnded = () => {
+    music.current.currentTime = 0;
+    music.current.play();
+  };
   return (
     <div className="titleWrapper">
       <div
-        className={`title ${isAbsolute ? "titleAbsolute" : ""}`}
+        className={`${!isBirthday && "title"} ${
+          isAbsolute ? "titleAbsolute" : isBirthday ? "birthday" : ""
+        }`}
         onClick={changeMusicStatus}
       >
-        Eilmoon
+        {text}
       </div>
-      <audio controls src={mp3} ref={music} style={{ display: "none" }}></audio>
+      <audio
+        controls
+        src={mp3}
+        ref={music}
+        style={{ display: "none" }}
+        onEnded={handleMusicEnded}
+      ></audio>
     </div>
   );
 }
