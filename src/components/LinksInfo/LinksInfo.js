884834fde171f3b78035e8a12e283dc78e3fe218
m09nlight <ivan.portyanko1609@gmail.com>
2024-05-03 21:47:51 +0300

    sad

--- src/components/LinksInfo/LinksInfo.js
+++ src/components/LinksInfo/LinksInfo.js
@@ -1,18 +1,18 @@
-import React, { useRef, useState } from "react";
-import mp3 from "../../love.mp3";
-import Title from "../Title/Title";
+import React, { useRef, useState } from 'react';
+import mp3 from '../../love.mp3';
+import Title from '../Title/Title';
 
 export default function LinksInfo() {
   return (
-    <div className="containerApp">
+    <div className='containerApp'>
       <Title mp3={mp3} />
-      <div className="button">
+      {/* <div className="button">
         <a href="https://instagram.com/eilmoon" target="blank" className="link">
           inst
           <img src="heart.png" alt="" className="heart" />
         </a>
-      </div>
-      <div className="button">
+      </div> */}
+      {/* <div className="button">
         <a
           href="https://t.me/+a3EyxtojIfEyMzli"
           target="blank"
@@ -20,12 +20,17 @@ export default function LinksInfo() {
         >
           tg
         </a>
+      </div> */}
+      <div className='button'>
+        <a href='https://t.me/m09nlight' target='blank' className='link'>
+          To the Moon ☾
+        </a>
       </div>
-      <div className="button">
+      <div className='button'>
         <a
-          href="https://www.donationalerts.com/r/eilmoon"
-          target="blank"
-          className="link"
+          href='https://www.donationalerts.com/r/eilmoon'
+          target='blank'
+          className='link'
         >
           donationalerts
         </a>
